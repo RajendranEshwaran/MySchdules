3e477b6e8554d1b74da09e345b811c84291b7f59
RAJENDRAN ESHWARAN <10699484+RajendranEshwaran@users.noreply.github.com>
2025-06-19 15:39:24 -0400

    Initial Commit

---
+++ MySchdules/MySchdulesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySchdulesApp.swift
+//  MySchdules
+//
+//  Created by RajayGoms on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct MySchdulesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MySchdulesTests/MySchdulesTests.swift
@@ -0,0 +1,17 @@
+//
+//  MySchdulesTests.swift
+//  MySchdulesTests
+//
+//  Created by RajayGoms on 6/19/25.
+//
+
+import Testing
+@testable import MySchdules
+
+struct MySchdulesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
